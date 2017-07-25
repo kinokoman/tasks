cd6720464e6ea1c234836a40a18665f6b86b8846
yuta <yuta@yutanoMacBook-Air.local>
2017-07-25 21:24:23 +0900

    課題2

---
+++ task2.py
@@ -0,0 +1,37 @@
+import time
+import csv
+
+start = time.time()
+
+f = open('large_text.txt')
+texts = f.readlines()
+f.close()
+
+for x in range(len(texts)):
+	texts[x]=texts[x].replace('\n','')
+	texts[x]=texts[x].replace('\r','')
+
+words=[]
+with open("./words.tsv") as rf:
+    reader = csv.reader(rf, delimiter="\t")
+    for row in reader:
+        words.append(row)
+
+answers=[]
+for x in range(len(texts)):
+	answer=0
+	for y in range(len(words)):
+		for z in range(len(texts[0])):
+			if words[y][0]==texts[x][z:z+len(words[y][0])]:
+				answer+=int(words[y][1])
+	answers.append(answer)
+	
+finish = time.time()
+time = finish - start
+
+f = open('answer1.txt', 'w')
+for say in answers:
+	f.write(str(say)+ "\n")
+
+f.write(str(time))
+f.close()
