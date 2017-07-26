5eb30b51dd23a59fa04c024998641d6bf72d92f6
kinokoman <kinokomann1031@gmail.com>
2017-07-27 01:02:11 +0900

    変数名とコメントを追加

--- task2.py
+++ task2.py
@@ -1,43 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import time
-import csv
 import re
 from multiprocessing import Pool
 
+#開始時間を記録
 start = time.time()
 
+#textファイルを開いてデータを読み込む
 f = open('large_text.txt')
 texts = f.readlines()
 f.close()
 
+#各テキストから余計な改行を省く
 for x in range(len(texts)):
 	texts[x]=texts[x].replace('\n','')
 	texts[x]=texts[x].replace('\r','')
 
-words=[]
-with open("./words.tsv") as rf:
-    reader = csv.reader(rf, delimiter="\t")
-    for row in reader:
-        words.append(row)
+#wordsファイルを開いてデータを読み込む
+f = open('words.tsv')
+words = f.readlines()
+f.close()
 
-def function(x):
+#各ワードから余計な改行を省いてワードと点数を分離
+for x in range(len(words)):
+	words[x]=words[x].replace('\n','')
+	words[x]=words[x].replace('\r','')
+	words[x]=words[x].split("\t")
+
+#テキストに各ワードが含まれているか検索して含まれている数だけ点数追加
+def serch(x):
 	answer=0
 	for y in range(len(words)):
 		answer+=int(words[y][1]) * len(re.findall('(?='+words[y][0]+')', texts[x]))
 	return answer
 
-def multi(n):
-    p = Pool(10)
-    result = p.map(function, range(n))
-    return result
+#並列処理を行う時の最大プロセス数を指定
+p = Pool(10)
 
-def main(hoge):
-    answers = multi(100)
-    finish = time.time()
-    zikan = finish - hoge
-    f = open('answer2_2.txt', 'w')
-    for say in answers:
-		f.write(str(say)+ "\n")
-    f.write(str(zikan))
-    f.close()
+#serch関数を並列処理で全テキスト同時に実行
+answers = p.map(serch, range(len(texts)))
 
-main(start)
+#終了時間を記録
+finish = time.time()
+
+#実行時間を算出
+time = finish - start
+
+#各テキストの点数と実行時間をanswerファイルに書き込む
+f = open('answer2_2.txt', 'w')
+for answer in answers:
+	f.write(str(answer)+ "\n")
+f.write(str(time))
+f.close()
